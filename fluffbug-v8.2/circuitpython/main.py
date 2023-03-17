04da18d9e3115b435c61ca7ac3c75b43521cde8d
Radomir Dopieralski <openstack@sheep.art.pl>
2023-03-17 21:42:24 +0100

    Blink LED during calibration
    To more easily see when all legs have moved to their positions,
    blink the LED while moving the legs, and keep the LED on when
    finished.

--- fluffbug-v8.2/circuitpython/main.py
+++ fluffbug-v8.2/circuitpython/main.py
@@ -3,15 +3,21 @@ import asyncio
 import time
 import walk
 import web
+import board
+import digitalio
 
 
 def calibrate():
+    led = digitalio.DigitalInOut(board.LED)
+    led.switch_to_output(value=0)
     for leg in robot.LEGS:
         leg.move()
+        led.value = not led.value
         time.sleep(0.25)
-
+    led.value = True
     while True:
         time.sleep(1)
+
 
 async def main():
     gait = walk.Walk()
